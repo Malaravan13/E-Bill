4df74cd3383a1ff1c838ad2534309e1a9e8a412b
Malaravan13 <malaravan264ro@gmail.com>
2025-08-25 13:35:32 +0530

    Updated Electricity_Bill.ipynb code

--- app.py
+++ app.py
@@ -2,21 +2,21 @@ import streamlit as st
 import pickle
 import numpy as np
 
-# Load trained model
+
 with open('trained_model.sav', 'rb') as f:
     model = pickle.load(f)
 
-# App title
+
 st.title("Electricity Bill Predictor ⚡")
 
 st.write("Enter the number of units consumed to predict the electricity bill amount.")
 
-# Input
+
 Units = st.number_input("Units Consumed:", min_value=0, max_value=10000, value=100)
 
 features = np.array([[Units]])
 
-# Prediction button
+
 if st.button("Predict Bill"):
     prediction = model.predict(features)
     st.success(f"Estimated Bill Amount: ₹{prediction[0]:,.2f}")
